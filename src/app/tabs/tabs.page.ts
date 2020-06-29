f7d2fa630638f77f981d3777302f6f9bd543ff71
Alexander Sitchikhin <108qwl@gmail.com>
2020-06-29 20:42:00 +0300

    Add stuff

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { Tab1Page } from '../tab1/tab1.page';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,14 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(
+    public modalCtrl: ModalController
+  ) {}
 
+  public openModal() {
+    this.modalCtrl.create({component: Tab1Page})
+      .then((modal) => {
+        modal.present();
+      });
+  }
 }
